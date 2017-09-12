b116fe49ec54a8b559c8c063ad5511fd1910ca61
recca0120 <recca0120@gmail.com>
2017-09-13 04:56:10 +0800

    polyfill

---
+++ src/polyfill.php
@@ -0,0 +1,23 @@
+<?php
+
+use Recca0120\Repository\HigherOrderTapProxy;
+
+if (! function_exists('tap')) {
+    /**
+     * Call the given Closure with the given value then return the value.
+     *
+     * @param  mixed  $value
+     * @param  callable|null  $callback
+     * @return mixed
+     */
+    function tap($value, $callback = null)
+    {
+        if (is_null($callback)) {
+            return new HigherOrderTapProxy($value);
+        }
+
+        $callback($value);
+
+        return $value;
+    }
+}
