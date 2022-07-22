39bbfd42f528ee317adb592716a14f9197614ad7
recca0120 <recca0120@gmail.com>
2022-07-22 19:26:01 +0800

    feat: use reflect to find methods

--- src/Criteria.php
+++ src/Criteria.php
@@ -4,21 +4,33 @@ namespace Recca0120\Repository;
 
 use BadMethodCallException;
 use Closure;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Support\Str;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionMethod;
 
+/**
+ * @mixin QueryBuilder
+ * @mixin EloquentBuilder
+ */
 class Criteria
 {
-    use Concerns\BuildsQueries,
-        Concerns\QueriesRelationships,
-        Concerns\EloquentBuildsQueries,
-        Concerns\SoftDeletingScope;
+    use Concerns\SoftDeletingScope;
 
+    protected $proxies = [
+        EloquentBuilder::class,
+        QueryBuilder::class,
+    ];
     /**
      * $methods.
      *
      * @var Method[]
      */
     protected $methods = [];
+
+    private static $cache = [];
 
     /**
      * Handle dynamic method calls into the method.
@@ -28,9 +40,17 @@ class Criteria
      * @return Criteria
      *
      * @throws BadMethodCallException
+     * @throws ReflectionException
      */
     public function __call($method, $parameters)
     {
+        $hasMethod = $this->hasMethod($method);
+        if ($hasMethod) {
+            $this->methods[] = new Method($method, $parameters);
+
+            return $this;
+        }
+
         if (Str::startsWith($method, 'where')) {
             return $this->dynamicWhere($method, $parameters);
         }
@@ -96,5 +116,50 @@ class Criteria
                 'parameters' => $method->parameters,
             ];
         }, $this->methods);
+    }
+
+    /**
+     * Begin querying the model on the write connection.
+     *
+     * @return Criteria
+     */
+    public function onWriteConnection()
+    {
+        return $this->useWritePdo();
+    }
+
+    /**
+     * @param string $class
+     * @return string[]
+     * @throws ReflectionException
+     */
+    private function findMethods($class)
+    {
+        if (array_key_exists($class, self::$cache)) {
+            return self::$cache[$class];
+        }
+
+        $ref = new ReflectionClass($class);
+
+        return self::$cache[$class] = array_flip(array_map(static function ($method) {
+            return $method->getName();
+        }, $ref->getMethods(ReflectionMethod::IS_PUBLIC)));
+    }
+
+    /**
+     * @param string $method
+     * @return bool
+     * @throws ReflectionException
+     */
+    private function hasMethod(string $method)
+    {
+        foreach ($this->proxies as $proxy) {
+            $methods = $this->findMethods($proxy);
+            if (array_key_exists($method, $methods)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
